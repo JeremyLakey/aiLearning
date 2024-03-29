be90b49ab8352a7f4decef854c62f525a8188c54
personman61 <jeremysl123@gmail.com>
2024-03-28 21:09:23 -0600

    Trying some more parsing stuff

--- Recipe-Parser/scraper.py
+++ Recipe-Parser/scraper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import json
+import collections
 
 options = Options()
 options.headless = True
@@ -11,16 +12,31 @@ driver=webdriver.Chrome(options=options)
 
 def scrap_recipe(url):
     driver.get(url)
+    print(" ")
+    print(url)
 
     eles = driver.find_elements(By.TAG_NAME, 'script')
     for e in eles:
         tex = e.get_attribute('innerText')
+
         if "\"recipeIngredient\"" in tex:
+            meta = json.loads(tex)
+            if type(meta) is dict:
+                if "name" in meta:
+                    print(meta["name"])
+                else:
+                    for key in meta["@graph"]:
+                        if key["@type"] == 'Recipe':
+                            print(key["name"])
+                            print(key["recipeIngredient"])
+                            for step in key["recipeInstructions"]:
+                                print(step['text'])
+            else:
+                print(meta[0]["name"])
             i = tex.find("\"recipeIngredient\"")
             j = tex[i:].find("]")
             result = json.loads( '{ ' + tex[i:i+j+1] + ' }')
             ingredients = result["recipeIngredient"]
-            print(" ")
             for ing in ingredients:
                 print(ing)
 
